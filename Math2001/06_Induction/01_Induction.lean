1d9d1499763a6e3304f827a66fc1298368e187b2
Keanu Nichols <nichols.keanu9@gmail.com>
2024-09-10 08:14:28 -0400

    Add files via upload

---
+++ Math2001/06_Induction/01_Induction.lean
@@ -0,0 +1,109 @@
+/- Copyright (c) Heather Macbeth, 2023.  All rights reserved. -/
+import Mathlib.Data.Real.Basic
+import Library.Basic
+import Library.Tactic.ModEq
+
+math2001_init
+
+namespace Nat
+
+
+example (n : ℕ) : 2 ^ n ≥ n + 1 := by
+  simple_induction n with k IH
+  · -- base case
+    numbers
+  · -- inductive step
+    calc 2 ^ (k + 1) = 2 * 2 ^ k := by ring
+      _ ≥ 2 * (k + 1) := by rel [IH]
+      _ = (k + 1 + 1) + k := by ring
+      _ ≥ k + 1 + 1 := by extra
+
+
+example (n : ℕ) : Even n ∨ Odd n := by
+  simple_induction n with k IH
+  · -- base case
+    sorry
+  · -- inductive step
+    obtain ⟨x, hx⟩ | ⟨x, hx⟩ := IH
+    · sorry
+    · sorry
+
+example {a b d : ℤ} (h : a ≡ b [ZMOD d]) (n : ℕ) : a ^ n ≡ b ^ n [ZMOD d] := by
+  sorry
+
+example (n : ℕ) : 4 ^ n ≡ 1 [ZMOD 15] ∨ 4 ^ n ≡ 4 [ZMOD 15] := by
+  simple_induction n with k IH
+  · -- base case
+    left
+    numbers
+  · -- inductive step
+    obtain hk | hk := IH
+    · right
+      calc (4:ℤ) ^ (k + 1) = 4 * 4 ^ k := by ring
+        _ ≡ 4 * 1 [ZMOD 15] := by rel [hk]
+        _ = 4 := by numbers
+    · left
+      calc (4:ℤ) ^ (k + 1) = 4 * 4 ^ k := by ring
+        _ ≡ 4 * 4 [ZMOD 15] := by rel [hk]
+        _ = 15 * 1 + 1 := by numbers
+        _ ≡ 1 [ZMOD 15] := by extra
+
+
+example {n : ℕ} (hn : 2 ≤ n) : (3:ℤ) ^ n ≥ 2 ^ n + 5 := by
+  induction_from_starting_point n, hn with k hk IH
+  · -- base case
+    numbers
+  · -- inductive step
+    calc (3:ℤ) ^ (k + 1) = 2 * 3 ^ k + 3 ^ k := by ring
+      _ ≥ 2 * (2 ^ k + 5) + 3 ^ k := by rel [IH]
+      _ = 2 ^ (k + 1) + 5 + (5 + 3 ^ k) := by ring
+      _ ≥ 2 ^ (k + 1) + 5 := by extra
+
+
+example : forall_sufficiently_large n : ℕ, 2 ^ n ≥ n ^ 2 := by
+  dsimp
+  use 4
+  intro n hn
+  induction_from_starting_point n, hn with k hk IH
+  · -- base case
+    sorry
+  · -- inductive step
+    sorry
+
+
+/-! # Exercises -/
+
+
+example (n : ℕ) : 3 ^ n ≥ n ^ 2 + n + 1 := by
+  sorry
+
+example {a : ℝ} (ha : -1 ≤ a) (n : ℕ) : (1 + a) ^ n ≥ 1 + n * a := by
+  sorry
+
+example (n : ℕ) : 5 ^ n ≡ 1 [ZMOD 8] ∨ 5 ^ n ≡ 5 [ZMOD 8] := by
+  sorry
+
+example (n : ℕ) : 6 ^ n ≡ 1 [ZMOD 7] ∨ 6 ^ n ≡ 6 [ZMOD 7] := by
+  sorry
+
+example (n : ℕ) :
+    4 ^ n ≡ 1 [ZMOD 7] ∨ 4 ^ n ≡ 2 [ZMOD 7] ∨ 4 ^ n ≡ 4 [ZMOD 7] := by
+  sorry
+
+example : forall_sufficiently_large n : ℕ, (3:ℤ) ^ n ≥ 2 ^ n + 100 := by
+  dsimp
+  sorry
+
+example : forall_sufficiently_large n : ℕ, 2 ^ n ≥ n ^ 2 + 4 := by
+  dsimp
+  sorry
+
+example : forall_sufficiently_large n : ℕ, 2 ^ n ≥ n ^ 3 := by
+  dsimp
+  sorry
+
+theorem Odd.pow {a : ℕ} (ha : Odd a) (n : ℕ) : Odd (a ^ n) := by
+  sorry
+
+theorem Nat.even_of_pow_even {a n : ℕ} (ha : Even (a ^ n)) : Even a := by
+  sorry
